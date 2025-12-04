0a517af4db4e242b8fa7cc72ebcad17ff42a8a94
SMB_INSPIRE_LAB_DOCENTE <smb_d2_docente@ecotec.ec>
2025-12-04 09:08:12 -0500

    Proyecto

---
+++ test_calculadora.py
@@ -0,0 +1,19 @@
+import pytest
+from calculadora import Calculadora
+
+@pytest.fixture
+def calc():
+    return Calculadora()
+
+def test_suma(calc):
+    assert calc.suma(2, 3) == 5
+
+def test_resta(calc):
+    assert calc.resta(5, 2) == 3
+
+def test_divide_normal(calc):
+    assert calc.divide(10, 2) == 5
+
+def test_divide_por_cero(calc):
+    with pytest.raises(ValueError):
+        calc.divide(10, 0)
